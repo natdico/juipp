ed0fe013b2699a8dce925fbb377d9764b1520982
natdico <natcrayon@gmail.com>
2013-06-07 14:57:56 -0400

    Visual Studio Extension Update

---
+++ Org.Juipp.Extension/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace AdiswareLLC.Org_Juipp_Extension
+{
+    static class GuidList
+    {
+        public const string guidOrg_Juipp_ExtensionPkgString = "dabb4628-c8f9-4df1-9689-50b387a1b280";
+        public const string guidOrg_Juipp_ExtensionCmdSetString = "9428683d-cb5a-4300-bd0e-c0d68d99309c";
+
+        public static readonly Guid guidOrg_Juipp_ExtensionCmdSet = new Guid(guidOrg_Juipp_ExtensionCmdSetString);
+    };
+}
---
+++ Org.Juipp.Setup/Content/VisualStudioSDK.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FirstFloor.ModernUI.Windows;
+using FirstFloor.ModernUI.Windows.Controls;
+
+namespace Org.Juipp.Setup.Content
+{
+    /// <summary>
+    /// Interaction logic for VisualStudioSDK.xaml
+    /// </summary>
+    public partial class VisualStudioSdk : UserControl, IContent
+    {
+        public VisualStudioSdk()
+        {
+            InitializeComponent();
+        }
+
+        #region IContent Members
+
+        public void OnFragmentNavigation(FirstFloor.ModernUI.Windows.Navigation.FragmentNavigationEventArgs e)
+        {
+            
+        }
+
+        public void OnNavigatedFrom(FirstFloor.ModernUI.Windows.Navigation.NavigationEventArgs e)
+        {
+
+        }
+
+        public void OnNavigatedTo(FirstFloor.ModernUI.Windows.Navigation.NavigationEventArgs e)
+        {
+
+        }
+
+        public void OnNavigatingFrom(FirstFloor.ModernUI.Windows.Navigation.NavigatingCancelEventArgs e)
+        {
+            if (false == ModernDialog.ShowMessage("Navigate away?", "navigate", System.Windows.MessageBoxButton.YesNo)) 
+            {
+                e.Cancel = true;
+            }
+        }
+
+        #endregion
+    }
+}
